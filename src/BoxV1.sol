bf2b0f124888bb3f2d523262460e3a1f6fa188c7
12anand34 <anand47bansal#gmail.com>
2025-03-13 15:29:53 +0000

    imports and constructor morphing

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,8 +2,23 @@
 
 pragma solidity ^0.8.18;
 
-contract BoxV1{
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable{
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender); //sets owner to msg.sender
+        __UUPSUpgradeable_init(); //Do nothing but convention
+    }
+
 
     function getNumber() external view returns (uint256){
         return number;
@@ -12,4 +27,6 @@ contract BoxV1{
     function version() public pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override  {}
 }
