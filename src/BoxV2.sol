7f7d077b13061b1b201034e4db1794894d770633
12anand34 <anand47bansal#gmail.com>
2025-03-13 15:01:07 +0000

    Box V1 and V2 core done

---
+++ src/BoxV2.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract BoxV2{
+    uint256 internal number;
+     
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+}
